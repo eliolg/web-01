2b5712590e6a5fb15c9c7d0b57816ce36b0394da
Your Name <you@example.com>
2024-04-10 14:25:58 +0200

    commit() : Finishing TP (component, transpiler)

--- init/front-end/src/main.js
+++ init/front-end/src/main.js
@@ -3,10 +3,15 @@ import { Router } from "./app/scripts/router/router.js";
 import { GameComponent } from "./app/components/game/game.component.js";
 import { WelcomeComponent} from "./app/components/welcome/welcome.component.js"
 import { ScoreComponent } from "./app/components/score/score.component.js";
+import { NavbarComponent } from "./app/components/navbar/navbar.component.js";
+import { FooterComponent } from "./app/components/footer/footer.component.js";
 // TODO #import-components: use ES named imports to import WelcomeComponent, GameComponent a ScoreComponent
 // TODO #import-css: use ES side-effect imports to import styles/style.css
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./app/styles/style.css";
+
+customElements.define("my-navbar", NavbarComponent);
+customElements.define("my-footer", FooterComponent);
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
